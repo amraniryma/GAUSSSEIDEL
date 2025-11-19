ad537881a29d352346c89eab1b7c7e4eb6532436
amraniryma <rabacha515@gamail.com>
2025-11-19 19:08:22 +0100

    Add Gauss-Seidel method

--- gauss_seidel.py
+++ gauss_seidel.py
@@ -8,4 +8,18 @@ def is_diagonally_dominant(A):
             return False
     return True
 # Function checks if matrix A is diagonally dominant
+def gauss_seidel(A, B, tol=1e-6, max_iterations=100):
+    n = len(A)
+    X = np.zeros(n, dtype=float)
+    for k in range(max_iterations):
+        X_old = X.copy()
+        for i in range(n):
+            sum_before = sum(A[i][j] * X[j] for j in range(i))
+            sum_after = sum(A[i][j] * X_old[j] for j in range(i+1, n))
+            X[i] = (B[i] - sum_before - sum_after) / A[i][i]
+        print(f"Iteration {k+1}: {X}")
+        if np.allclose(X, X_old, atol=tol):
+            return X
+    return X
+# Function solves AX=B using Gauss-Seidel iterative method
 
